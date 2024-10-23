534640b698d6aa945cbd7e15c9e3c4f8cf6bea83
jhonrneilebuan <jhonrneilebuan@gmail.com>
2024-10-24 00:29:13 +0800

    Update contact_bloc.dart

--- lib/bloc/contact/contact_bloc.dart
+++ lib/bloc/contact/contact_bloc.dart
@@ -2,8 +2,7 @@ import 'package:aj_autofix/bloc/contact/contact_event.dart';
 import 'package:aj_autofix/bloc/contact/contact_state.dart';
 import 'package:aj_autofix/repositories/contact_repository_impl.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-
+import 'package:aj_autofix/utils/secure_storage.dart';
 
 class ContactBloc extends Bloc<ContactEvent, ContactState> {
   final ContactRepositoryImpl contactRepository;
@@ -16,7 +15,19 @@ class ContactBloc extends Bloc<ContactEvent, ContactState> {
     emit(ContactSubmitting());
 
     try {
-      final isSuccess = await contactRepository.sendContact(event.name, event.email, event.message);
+      final accessToken = await SecureStorage.readToken('access_token');
+
+      if (accessToken == null) {
+        emit(ContactFailure('No access token found.'));
+        return;
+      }
+
+      final isSuccess = await contactRepository.sendContact(
+        event.name,
+        event.email,
+        event.message,
+      );
+
       if (isSuccess) {
         emit(ContactSuccess());
       } else {
