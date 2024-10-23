e47a4d95115a3b98339f4a42084aa732a4177be3
jhonrneilebuan <jhonrneilebuan@gmail.com>
2024-10-24 00:29:08 +0800

    Update contact_repository_impl.dart

--- lib/repositories/contact_repository_impl.dart
+++ lib/repositories/contact_repository_impl.dart
@@ -1,14 +1,21 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../utils/constants.dart';
+import '../utils/secure_storage.dart';
+
 class ContactRepositoryImpl {
-
-
   Future<bool> sendContact(String name, String email, String message) async {
+    final accessToken = await SecureStorage.readToken('access_token');
+
+    if (accessToken == null) {
+      throw Exception('No access token found');
+    }
+
     final response = await http.post(
       Uri.parse('${ApiConstants.baseUrl}/contacts/contacts'),
       headers: {
         'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken',
       },
       body: jsonEncode({
         'name': name,
@@ -19,6 +26,8 @@ class ContactRepositoryImpl {
 
     if (response.statusCode == 200) {
       return true;
+    } else if (response.statusCode == 401) {
+      throw Exception('Unauthorized: Invalid access token.');
     } else {
       throw Exception('Failed to send contact message: ${response.body}');
     }
