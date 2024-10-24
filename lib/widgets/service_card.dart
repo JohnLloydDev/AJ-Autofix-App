ed54443cbbe2ccc57a5485724d8e7c881ef2e0f0
jhonrneilebuan <jhonrneilebuan@gmail.com>
2024-10-24 22:51:33 +0800

    Update service_card.dart

--- lib/widgets/service_card.dart
+++ lib/widgets/service_card.dart
@@ -21,7 +21,8 @@ class ServiceCard extends StatefulWidget {
   ServiceCardState createState() => ServiceCardState();
 }
 
-class ServiceCardState extends State<ServiceCard> with SingleTickerProviderStateMixin {
+class ServiceCardState extends State<ServiceCard>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _scaleAnimation;
 
@@ -32,7 +33,8 @@ class ServiceCardState extends State<ServiceCard> with SingleTickerProviderState
       vsync: this,
       duration: const Duration(milliseconds: 300),
     );
-    _scaleAnimation = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+    _scaleAnimation =
+        CurvedAnimation(parent: _controller, curve: Curves.easeOut);
     _controller.forward();
   }
 
@@ -52,11 +54,12 @@ class ServiceCardState extends State<ServiceCard> with SingleTickerProviderState
           child: LayoutBuilder(
             builder: (context, constraints) {
               double width = constraints.maxWidth;
+              double height = width * 0.80;
 
-              double imageHeight = width * 0.29;
+              double imageHeight = height * 0.40;
               double imageWidth = width * 0.45;
               double fontSize = width * 0.07;
-              double buttonSize = width * 0.12;
+              double buttonSize = width * 0.13;
 
               return Card(
                 elevation: 5,
@@ -65,7 +68,7 @@ class ServiceCardState extends State<ServiceCard> with SingleTickerProviderState
                   borderRadius: BorderRadius.circular(16.0),
                 ),
                 child: Padding(
-                  padding: const EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(8.0),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -80,21 +83,61 @@ class ServiceCardState extends State<ServiceCard> with SingleTickerProviderState
                             fit: BoxFit.cover,
                           ),
                         ),
-                      const SizedBox(height: 12),
-                      
+                      const SizedBox(height: 8),
                       Text(
                         widget.serviceName.replaceAllMapped(
-                          RegExp(r'(Power Window Motor Installation|Powerlock Set Installation|Fuel Injector Cleaning Service|Alternator Repair Service)'),
+                          RegExp(
+                              r'(Power Window Motor Installation|Power Window Cable Replacement|Powerlock \(1pc\) Installation|Powerlock Set Installation|Door Lock Replacement|Door Handle Replacement|Door Handle Repair Service|Door Lock Repair Service|Coolant Flush Service|Engine Oil Change Service|Spark Plug Replacement|Air Filter Replacement|Fuel Injector Cleaning Service|Timing Belt Replacement|Tire Replacement Service|Wheel Alignment Service|Brake Pad Set Replacement|Brake Fluid Replacement|Alternator Repair Service|Fuse Replacement Service|Car Alarm Installation|Battery Replacement Service|Headlight Bulb Replacement|Power Window Switch Replacement)'),
                           (match) {
                             switch (match.group(0)) {
                               case 'Power Window Motor Installation':
                                 return 'Power Window Motor\nInstallation';
+                              case 'Power Window Cable Replacement':
+                                return 'Power Window\nCable Replacement';
+                              case 'Powerlock (1pc) Installation':
+                                return 'Powerlock (1pc)\nInstallation';
                               case 'Powerlock Set Installation':
                                 return 'Powerlock Set\nInstallation';
+                              case 'Door Lock Replacement':
+                                return 'Door Lock\nReplacement';
+                              case 'Door Handle Replacement':
+                                return 'Door Handle\nReplacement';
+                              case 'Door Handle Repair Service':
+                                return 'Door Handle\nRepair Service';
+                              case 'Door Lock Repair Service':
+                                return 'Door Lock\nRepair Service';
+                              case 'Coolant Flush Service':
+                                return 'Coolant\nFlush Service';
+                              case 'Engine Oil Change Service':
+                                return 'Engine Oil Change\nService';
+                              case 'Spark Plug Replacement':
+                                return 'Spark Plug\nReplacement';
+                              case 'Air Filter Replacement':
+                                return 'Air Filter\nReplacement';
                               case 'Fuel Injector Cleaning Service':
                                 return 'Fuel Injector\nCleaning Service';
+                              case 'Timing Belt Replacement':
+                                return 'Timing Belt\nReplacement';
+                              case 'Tire Replacement Service':
+                                return 'Tire Replacement\nService';
+                              case 'Wheel Alignment Service':
+                                return 'Wheel Alignment\nService';
+                              case 'Brake Pad Set Replacement':
+                                return 'Brake Pad Set\nReplacement';
+                              case 'Brake Fluid Replacement':
+                                return 'Brake Fluid\nReplacement';
                               case 'Alternator Repair Service':
                                 return 'Alternator Repair\nService';
+                              case 'Fuse Replacement Service':
+                                return 'Fuse Replacement\nService';
+                              case 'Car Alarm Installation':
+                                return 'Car Alarm\nInstallation';
+                              case 'Battery Replacement Service':
+                                return 'Battery Replacement\nService';
+                              case 'Headlight Bulb Replacement':
+                                return 'Headlight Bulb\nReplacement';
+                              case 'Power Window Switch Replacement':
+                                return 'Power Window Switch\nReplacement';
                               default:
                                 return match.group(0)!;
                             }
@@ -107,7 +150,7 @@ class ServiceCardState extends State<ServiceCard> with SingleTickerProviderState
                           color: Colors.black,
                         ),
                       ),
-                      const SizedBox(height: 8),
+                      const SizedBox(height: 6),
                       FittedBox(
                         child: Text(
                           widget.servicePrice,
@@ -131,14 +174,18 @@ class ServiceCardState extends State<ServiceCard> with SingleTickerProviderState
                               toastLength: Toast.LENGTH_SHORT,
                               gravity: ToastGravity.BOTTOM,
                               timeInSecForIosWeb: 1,
-                              backgroundColor: const Color.fromARGB(100, 0, 0, 0),
+                              backgroundColor:
+                                  const Color.fromARGB(100, 0, 0, 0),
                               textColor: Colors.white,
                               fontSize: 16.0,
                             );
                           },
                           icon: Icon(
-                            widget.isSelected ? Icons.check_circle : Icons.add_circle,
-                            color: widget.isSelected ? Colors.green : Colors.black,
+                            widget.isSelected
+                                ? Icons.check_circle
+                                : Icons.add_circle,
+                            color:
+                                widget.isSelected ? Colors.green : Colors.black,
                             size: buttonSize,
                           ),
                         ),
