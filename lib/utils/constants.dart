b9572f3a3c563bcc83b91b5b6ea06b1edb05570a
jhonrneilebuan <jhonrneilebuan@gmail.com>
2024-10-24 20:30:30 +0800

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -9,12 +9,13 @@ const double kSpacing = 16.0;
 
 const Color kPrimaryColor = Colors.lightBlue;
 const Color kAccentColor = Colors.white;
-const Color kMainColor = Color(0xFF6E88A1);
+const Color kMainColor = Color.fromRGBO(110, 136, 161, 1);
 const Color kRedColor = Color.fromARGB(255, 255, 0, 34);
-const Color kColor = Color.fromARGB(255, 110, 136, 161);
+const Color kColor = Color.fromARGB(255, 146, 176, 204);
 const Color kGreenColor = Color.fromRGBO(76, 175, 80, 1);
 const Color kBlackColor = Color(0xFF000000);
 const Color kBlueColor = Color.fromRGBO(0, 122, 255, 1);
+const Color kdarkColor = Color.fromRGBO(0, 31, 63, 1);
 
 const BorderRadius kBorderRadius = BorderRadius.all(Radius.circular(8.0));
 
